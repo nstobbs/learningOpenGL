81238fb5fb98cca9013da71b3851934291fbc3e1
Nathan Stobbs <nstobbs97@gmail.com>
2021-05-12 22:11:36 +0100

    adding new dependenies working on xcode

--- learningOpenGL/App.cpp
+++ learningOpenGL/App.cpp
@@ -1,3 +1,4 @@
+#define GL_SILENCE_DEPRECATION
 #include <GLFW/glfw3.h>
 
 int main(void)
@@ -15,7 +16,7 @@ int main(void)
         glfwTerminate();
         return -1;
     }
-
+	
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
